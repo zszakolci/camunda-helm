578c4ebeabe73db5c6bd37043e0de8d4f302b636
Zalán Szakolci <zalan.szakolci@camunda.com>
2023-09-29 14:46:42 +0800

    added some windows support (not tested)

---
+++ pull_camunda_windows.bat
@@ -0,0 +1,11 @@
+ docker pull camunda/connectors-bundle:0.21.5
+ docker pull docker.elastic.co/elasticsearch/elasticsearch:7.17.10
+ docker pull registry.camunda.cloud/web-modeler-ee/modeler-webapp:8.2.6
+ docker pull registry.camunda.cloud/web-modeler-ee/modeler-restapi:8.2.6
+ docker pull registry.camunda.cloud/web-modeler-ee/modeler-websockets:8.2.6
+ docker pull bitnami/postgresql:15.3.0
+
+docker pull camunda/operate:8.2.15
+docker pull camunda/identity:8.2.15
+docker pull camunda/tasklist:8.2.15
+docker pull camunda/zeebe:8.2.15
--- start_from_zero_windows.bat
+++ start_from_zero_windows.bat
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 DOCKER_BUILDKIT=0 docker build -t keycloak-arm "https://github.com/camunda/camunda-platform.git#main:.keycloak/"
-./pull_camunda.sh
-./spinup.sh
+pull_camunda_windows.bat
+spinup_windows.bat
